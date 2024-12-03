b3a05c73948c2b6317c8f80a095d2025140097e2
yamato <yamato1212810@icloud.com>
2024-12-03 20:15:45 +0900

    ee

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,83 +3,77 @@ import { Toaster } from "@/components/ui/toaster";
 
 import { cn } from "@/lib/utils";
 
-import type { Metadata } from "next";
+
 import NextTopLoader from "nextjs-toploader";
 
 import { Inter } from "next/font/google";
 import "../../globals.css";
-import { siteConfig } from "../config/site";
-import { Suspense } from "react";
-import GoogleAnalytics from "@/components/GoogleAnalytics/GoogleAnalytics";
 
-import GoogleAdsense from "@/components/GoogleAdsense/GoogleAdsense";
-import { Header } from "@/layouts/header";
-import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  metadataBase: new URL(siteConfig.url),
-  description: siteConfig.description,
-  keywords: [
-    "Next.js",
-    "React",
-    "Tailwind CSS",
-    "Server Components",
-    "Radix UI",
-  ],
-  authors: [
-    {
-      name: "yamato",
-      url: "https://casiry.com",
-    },
-  ],
-  creator: "yamato",
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-    images: [
-      {
-        url: siteConfig.ogImage,
-        width: 1200,
-        height: 630,
-        alt: siteConfig.name,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [siteConfig.ogImage],
-    creator: "@shadcn",
-  },
-  icons: {
-    icon: "/apps/web/src/app/[lang]/icon.ico",
-  },
-};
+// export const metadata: Metadata = {
+//   title: {
+//     default: siteConfig.name,
+//     template: `%s - ${siteConfig.name}`,
+//   },
+//   metadataBase: new URL(siteConfig.url),
+//   description: siteConfig.description,
+//   keywords: [
+//     "Next.js",
+//     "React",
+//     "Tailwind CSS",
+//     "Server Components",
+//     "Radix UI",
+//   ],
+//   authors: [
+//     {
+//       name: "yamato",
+//       url: "https://casiry.com",
+//     },
+//   ],
+//   creator: "yamato",
+//   openGraph: {
+//     type: "website",
+//     locale: "en_US",
+//     url: siteConfig.url,
+//     title: siteConfig.name,
+//     description: siteConfig.description,
+//     siteName: siteConfig.name,
+//     images: [
+//       {
+//         url: siteConfig.ogImage,
+//         width: 1200,
+//         height: 630,
+//         alt: siteConfig.name,
+//       },
+//     ],
+//   },
+//   twitter: {
+//     card: "summary_large_image",
+//     title: siteConfig.name,
+//     description: siteConfig.description,
+//     images: [siteConfig.ogImage],
+//     creator: "@shadcn",
+//   },
+//   icons: {
+//     icon: "/apps/web/src/app/[lang]/icon.ico",
+//   },
+// };
 
 export default async function RootLayout({
   children,
-  authModal,
+  
 }: {
   children: React.ReactNode;
-  authModal: React.ReactNode;
+  
 }) {
   return (
     <html lang="jp" suppressHydrationWarning>
       <head>
-        <Suspense>
+        {/* <Suspense>
           <GoogleAnalytics />
-        </Suspense>
+        </Suspense> */}
       </head>
       <body className={cn(inter.className)}>
         <NextTopLoader
@@ -98,14 +92,13 @@ export default async function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <SessionProvider>
-            {authModal}
+        
             <div className="h-full">{children}</div>
-          </SessionProvider>
+        
         </ThemeProvider>
         <Toaster />
       </body>
-      <GoogleAdsense />
+      {/* <GoogleAdsense /> */}
     </html>
   );
 }
